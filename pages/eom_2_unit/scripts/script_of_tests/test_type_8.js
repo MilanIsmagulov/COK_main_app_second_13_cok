fed0f29d2a1661bb9246c2a3d99756e2489f5156
Milan Ismagulov <4k.milan15@gmail.com>
2024-11-18 13:49:51 +0500

    18.11.2024 13.49

--- pages/eom_2_unit/scripts/script_of_tests/test_type_8.js
+++ pages/eom_2_unit/scripts/script_of_tests/test_type_8.js
@@ -13,12 +13,22 @@ if (attempts == 0){
 };
 if (!planeText) {
     function createTest(index){
-        if (blockButtonEOM2 === 1 && attempts != 0){
-            backWardBtn.classList.add('gray_dis');
-            backWardBtn.disabled = true;
-            nextBtn.classList.add('gray_dis');
-            nextBtn.disabled = true;
+        function checkBtnStatus(){
+            var testData = data[`index_${currentPageIndex}`];
+            var attempts = parseInt(localStorage.getItem(`attempts_${currentPageIndex}`));
+            if(blockButtonEOM2 == 1 && attempts !== 0 && testData.hasOwnProperty('test')){
+                backWardBtn.classList.add('gray_dis');
+                backWardBtn.disabled = true;
+                nextBtn.classList.add('gray_dis');
+                nextBtn.disabled = true;
+            } else {
+                backWardBtn.classList.remove('gray_dis');
+                backWardBtn.disabled = false;
+                nextBtn.classList.remove('gray_dis');
+                nextBtn.disabled = false;
+            }
         }
+        checkBtnStatus();
         var test = data[index].test;
         if (test.paragraph_1){
             document.getElementById('control_button_1').classList.remove('gray_dis');
